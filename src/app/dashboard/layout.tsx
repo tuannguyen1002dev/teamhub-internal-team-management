b03456621ca627038c2474c9cf7c324fdedd2805
tuannguyen1002dev <tuannguyen1002dev@gmail.com>
2025-06-03 16:16:02 +0700

    fix layer

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,13 +9,31 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex flex-col flex-1">
-        <Navbar />
-        <main className="p-6 overflow-y-auto bg-gray-50 flex-1">
-          {children}
-        </main>
+    // <div className="flex h-screen w-screen bg-gradient-to-b from-black via-[#2c0a72] to-purple-600 text-white overflow-hidden">
+    //   <aside className="hidden md:block md:w-64 bg-[#1e1e2f] p-5 shadow-lg">
+    //     <Sidebar />
+    //   </aside>
+
+    //   <div className="flex flex-col flex-1">
+    //     <div className="md:hidden p-4 bg-[#1e1e2f]">
+    //       <button className="text-sm px-4 py-2 bg-purple-600 hover:bg-purple-700 rounded">Open Menu</button>
+    //     </div>
+
+    //     <header className="p-4 bg-[#1a1a2b] shadow-md">
+    //       <Navbar />
+    //     </header>
+
+    //     <main className="flex-1 overflow-y-auto bg-white bg-opacity-5 backdrop-blur-md p-6 rounded-t-2xl md:rounded-none">
+    //       {children}
+    //     </main>
+    //   </div>
+    // </div>
+    <div className="h-screen w-screen bg-white p-6 grid grid-cols-12 gap-6">
+      <div className="w-100 col-span-3 bg-amber-500">
+        {/* <Sidebar /> */}
+      </div>
+      <div className="w-100 col-span-full bg-violet-500">
+        {/* <Navbar /> */}
       </div>
     </div>
   )
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 bg-white shadow-md">
-      <div className="p-6 font-bold text-xl">MyDashboard</div>
+    <aside className=" bg-gray-950 p-3 flex flex-col gap-3 rounded-2xl">
+      <div className=" flex justify-center items-cente p-2">
+        <div className="text-2xl font-bold text-violet-800">TeamHub</div>
+      </div>
       <nav>
         <ul className="space-y-2 px-4">
           <li><a href="#" className="block py-2 px-3 rounded hover:bg-gray-200">Dashboard</a></li>
