c18c425747cf1d8e2963a0c27bcbda3ee07be314
tuannguyen1002dev <tuannguyen1002dev@gmail.com>
2025-06-10 11:30:19 +0700

    update: NavItem logic

---
+++ src/components/dist/sidebar.js
@@ -0,0 +1,42 @@
+'use client';
+"use strict";
+exports.__esModule = true;
+exports.NavbarItem = void 0;
+var react_1 = require("react");
+// ** import icons
+var lucide_react_1 = require("lucide-react");
+var navigation = [
+    {
+        title: 'Dashboards',
+        icon: lucide_react_1.LayoutDashboard,
+        children: [
+            { title: 'CRM', path: '/dashboards/crm' },
+            { title: 'Analytics', path: '/dashboards/analytics' },
+            { title: 'eCommerce', path: '/dashboards/ecommerce' }
+        ]
+    },
+    { sectionTitle: 'Apps & Pages' },
+    { title: 'Email', icon: lucide_react_1.Mail, path: '/apps/email' },
+];
+function NavbarItem(props) {
+    var sideBarState = props.sideBarState;
+    console.log('tuan', navigation);
+    return (react_1["default"].createElement("div", { className: "flex-1 bg-amber-800" }, navigation.map(function (items, index) {
+        return (react_1["default"].createElement("div", { className: "" },
+            react_1["default"].createElement("div", { className: "" }, 'sectionTitle' in items ?
+                react_1["default"].createElement("div", { className: "flex flex-row justify-center items-center gap-1" },
+                    react_1["default"].createElement("span", { className: "h-[1px] w-[20px] bg-gray-300" }),
+                    react_1["default"].createElement("div", { className: "text-white text-xs font-semibold" }, items.sectionTitle),
+                    react_1["default"].createElement("span", { className: "h-[1px] w-[20px] bg-gray-300" })) :
+                react_1["default"].createElement("div", null, 'children' in items
+                    ? react_1["default"].createElement("div", null)
+                    : react_1["default"].createElement("div", null)))));
+    })));
+}
+exports.NavbarItem = NavbarItem;
+function Sidebar(props) {
+    var sideBarState = props.sideBarState;
+    return (react_1["default"].createElement("div", { className: "flex-1 p-6" },
+        react_1["default"].createElement(NavbarItem, { sideBarState: sideBarState })));
+}
+exports["default"] = Sidebar;
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,18 +3,93 @@
 import React, { useState } from 'react'
 
 // ** import icons
-import { ChevronDown, LayoutDashboard } from 'lucide-react'
+import { ChevronDown, LayoutDashboard, Mail } from 'lucide-react'
+
+// !! Nav types
+
+export type NavLink = {
+  icon?: any
+  path?: string
+  title: string
+  disabled?: boolean
+  externalLink?: boolean
+  openInNewTab?: boolean
+}
+
+export type NavGroup = {
+  icon?: any
+  title: string
+  children?: (NavGroup | NavLink)[]
+}
+
+export type NavSectionTitle = {
+  subject?: string
+  sectionTitle: string
+}
+
+export type VerticalNavItemsType = (NavLink | NavGroup | NavSectionTitle)[]
+
+const navigation: VerticalNavItemsType = [
+  {
+    title: 'Dashboards',
+    icon: LayoutDashboard,
+    children: [
+      { title: 'CRM', path: '/dashboards/crm' },
+      { title: 'Analytics', path: '/dashboards/analytics' },
+      { title: 'eCommerce', path: '/dashboards/ecommerce' }
+    ]
+  },
+  { sectionTitle: 'Apps & Pages' },
+  { title: 'Email', icon: Mail, path: '/apps/email' },
+]
+
+
+
+export function NavbarItem(props: { sideBarState: boolean }) {
+  const { sideBarState } = props
+
+  console.log('tuan', navigation)
+
+  return (
+    <div className="flex-1 bg-amber-800">
+      {/* <button className="flex flex-row justify-start items-center">
+        <LayoutDashboard /> <span className={`font-semibold flex-shrink-0 transition-all duration-500 ease-in-out ${sideBarState ? 'w-0 opacity-0 overflow-hidden' : 'w-30 opacity-100 delay-150'}`}>Dashboard</span>
+      </button> */}
+      {navigation.map((items, index: number) => {
+        return (
+          <div className="">
+            <div className="">
+              {'sectionTitle' in items ?
+                <div className="flex flex-row justify-center items-center gap-1">
+                  <span className="h-[1px] w-[20px] bg-gray-300"></span>
+                  <div className="text-white text-xs font-semibold">
+                    {items.sectionTitle}
+                  </div>
+                  <span className="h-[1px] w-[20px] bg-gray-300"></span>
+                </div> :
+                <div>
+                  {'children' in items
+                    ? <div>
+
+                    </div>
+                    : <div>
+                      
+                    </div>}
+                </div>}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 
 export default function Sidebar(props: { sideBarState: boolean }) {
   const { sideBarState } = props
 
   return (
     <div className="flex-1 p-6">
-      <div className="flex flex-col gap-3">
-        <button className="flex flex-row justify-start items-center">
-          <LayoutDashboard /> <span className={`font-semibold flex-shrink-0 transition-all duration-500 ease-in-out ${sideBarState ? 'w-0 opacity-0 overflow-hidden' : 'w-30 opacity-100 delay-150'}`}>Dashboard</span>
-        </button>
-      </div>
+      <NavbarItem sideBarState={sideBarState} />
     </div>
   )
 }
