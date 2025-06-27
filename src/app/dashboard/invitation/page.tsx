a730cd8be618cf4c255878bee20cb76b3be3c92e
tuannguyen1002dev <tuannguyen1002dev@gmail.com>
2025-06-27 22:08:13 +0700

    Update invitationToken panel & Add post|fetch data

--- src/app/dashboard/invitation/page.tsx
+++ src/app/dashboard/invitation/page.tsx
@@ -1,36 +1,17 @@
 'use client'
 
 import Api from "@/shared/api";
-import React, { JSX, useEffect, useState } from "react";
+import React, { JSX, useEffect, useState, useRef } from "react";
 
-const invitaionCodeArray = [
-  {
-    id: 1,
-    code: "INV12345",
-    status: "Pending",
-    createdAt: "2023-10-01",
-    createBy: "Admin",
-  },
-  {
-    id: 2,
-    code: "INV67890",
-    status: "Accepted",
-    createdAt: "2023-10-02",
-    createBy: "Admin",
-  },
-  {
-    id: 3,
-    code: "INV54321",
-    status: "Expired",
-    createdAt: "2023-10-03",
-    createBy: "Admin",
-  },
-];
+import { Copy } from "lucide-react";
 
-const UserList = () => {
+export default function InvitationPanel() {
+
+  const inputEmailRef = useRef<HTMLInputElement | null>(null);
 
   const [invCodeCheck, setInvCodeCheck] = useState<string | null>("");
   const [invitaionCodeArray, setInvitaionCodeArray] = useState<any[]>([]);
+  const [recentGeneratedToken, setRecentGeneratedToken] = useState<string | null>(null);
 
   async function CopyInvCodeToClipboard(targetValue: string) {
     try {
@@ -69,6 +50,38 @@ const UserList = () => {
     fetchInvitationCodes()
   }, []);
 
+
+  // ** utils functions
+  function formatDateTime(dateInput: string | Date): string {
+    const date = new Date(dateInput);
+
+    const day = String(date.getDate()).padStart(2, '0');         // DD
+    const month = String(date.getMonth() + 1).padStart(2, '0');  // MM (0-based)
+    const year = date.getFullYear();                             // YYYY
+
+    const hours = String(date.getHours()).padStart(2, '0');      // hh
+    const minutes = String(date.getMinutes()).padStart(2, '0');  // mm
+    const seconds = String(date.getSeconds()).padStart(2, '0');  // ss
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  }
+
+  async function handleGenerateCode(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    // console.log("Generating code...", inputEmailRef.current?.value);
+    try {
+      Api.post('/invitation-token', { email: inputEmailRef.current?.value }).then((res: { data: any; }) => {
+        console.log(res.data);
+        setRecentGeneratedToken(res.data.token);
+        fetchInvitationCodes();
+      }).catch((err: any) => {
+        console.error("Error generating code:", err);
+      });
+    } catch (error) {
+      console.error("Error generating code:", error);
+    }
+  }
+
   return (
     <div className="flex flex-col gap-3 p-3">
       <h1 className="text-2xl font-bold mb-4">Code List</h1>
@@ -76,28 +89,28 @@ const UserList = () => {
         <table className="min-w-full bg-black border border-gray-200">
           <thead>
             <tr className="text-start">
-              <th className="px-4 py-2 border">ID</th>
-              <th className="px-4 py-2 border text-start">Code</th>
-              <th className="px-4 py-2 border text-start">Status</th>
-              <th className="px-4 py-2 border text-start">Created time</th>
+              <th className="px-4 py-2 border text-start">Access token</th>
+              <th className="px-4 py-2 border text-start">email</th>
+              <th className="px-4 py-2 border text-center">Status</th>
+              <th className="px-4 py-2 border text-center">Created time</th>
+              <th className="px-4 py-2 border text-center">Expires time</th>
               <th className="px-4 py-2 border text-start">Created by</th>
             </tr>
           </thead>
           <tbody>
-
-            {/* !! FIX THIS TABLE TO FIT WITH DATABSE CHECK FIELD */}
-
             {Array.isArray(invitaionCodeArray) && invitaionCodeArray.map(items => (
               <tr key={items.id} className="hover:bg-gray-900 relative group overflow-visible h-15">
-                <td className="px-4 py-2 border text-center">{items.id}</td>
-                <td className="px-4 py-2 border-t flex flex-row gap-3 justify-center items-center h-15" onClick={() => CopyInvCodeToClipboard(items.code)}>{items.code}
+                <td className="pl-4 py-2 border-t flex flex-row gap-3 justify-start items-center h-15" onClick={() => CopyInvCodeToClipboard(items.token)}>
+                  <span className="truncate w-50">{items.token}</span>
                   {invCodeCheck == items.code
                     ? <span className="p-1 bg-green-500/20">copied</span>
                     : <span className=" bg-red-500/20"></span>}
                 </td>
-                <td className="px-4 py-2 border">{items.status}</td>
-                <td className="px-4 py-2 border">{items.createdAt}</td>
-                <td className="px-4 py-2 border">
+                <td className="px-4 py-2 border w-150">{items.email}</td>
+                <td className="px-4 py-2 border w-40 text-center">{items.isUsed ? 'in active' : 'available'}</td>
+                <td className="px-4 py-2 border w-40 text-center">{formatDateTime(items.createdAt)}</td>
+                <td className="px-4 py-2 border w-40 text-center">{formatDateTime(items.expiresAt)}</td>
+                <td className="px-4 py-2 border w-60">
                   <span>{items.createBy}</span>
                 </td>
               </tr>
@@ -105,13 +118,20 @@ const UserList = () => {
           </tbody>
         </table>
       </div>
-      <div className="flex flex-row justify-end items-center">
-        <button onClick={generateCode}>
-          Generate code
-        </button>
+      <div className="flex flex-row gap-3 mt-4 w-full justify-end items-center">
+        <div className={`relative w-100 transition-all duration-500 ease-in-out ${recentGeneratedToken ? 'opacity-100' : 'opacity-0'}`}>
+          <input type="text" placeholder="access token" className="w-full pr-20 pl-4 py-2 border-gray-300 rounded-4xl border-0 outline-none focus:outline-none" disabled />
+          <button className="absolute right-1 top-1 bottom-1 px-4 bg-blue-500 text-white rounded-2xl hover:bg-blue-600">
+            <Copy size={16} />
+          </button>
+        </div>
+        <div className="relative w-100">
+          <input ref={inputEmailRef} type="text" placeholder="Enter new user email" className="w-full pr-20 pl-4 py-2 border border-gray-300 rounded-4xl focus:outline-none" />
+          <button className="absolute right-1 top-1 bottom-1 px-4 bg-blue-500 text-white rounded-2xl hover:bg-blue-600" onClick={(e) => handleGenerateCode(e)}>
+            Generate
+          </button>
+        </div>
       </div>
     </div>
   );
 };
-
-export default UserList;
