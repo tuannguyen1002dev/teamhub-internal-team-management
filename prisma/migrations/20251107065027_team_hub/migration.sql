7c47d8fa9729804d8fa3a424fc8a6bb21240dfa8
tuannguyen1002dev <tuannguyen1002dev@gmail.com>
2025-11-07 17:10:18 +0700

    update prisma rewrite into sqlite

---
+++ prisma/migrations/20251107065027_team_hub/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "fullname" TEXT,
+    "username" TEXT,
+    "dateOfBirth" DATETIME,
+    "phoneNumber" TEXT,
+    "address" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'USER,ADMIN,OPERATOR,SUDO',
+    "permissions" TEXT NOT NULL DEFAULT 'READ,WRITE,DELETE',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
