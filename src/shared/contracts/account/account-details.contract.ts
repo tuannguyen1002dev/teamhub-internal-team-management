d2a69cef93bc98bdd1eca3ea3213244d46b4592f
tuannguyen1002dev <tuannguyen1002dev@gmail.com>
2026-01-17 13:57:40 +0700

    update: restricted account contracts

--- src/shared/contracts/account/account-details.contract.ts
+++ src/shared/contracts/account/account-details.contract.ts
@@ -1,9 +1,10 @@
 export interface AccountDetailProps {
-  id: string
-  emial: string
+  id?: string
+  email: string
   username: string
   fullname: string
   phone: string
   address: string
-  dateofbirth: string
+  password: string,
+  confirmPassword: string
 }
