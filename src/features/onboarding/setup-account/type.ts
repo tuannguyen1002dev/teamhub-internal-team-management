c7c01d153d404165c3b89894bfb078b2dae54e36
tuannguyen1002dev <tuannguyen1002dev@gmail.com>
2026-01-17 13:58:15 +0700

    update type & defaultvalues of setup new userinputform

---
+++ src/features/onboarding/setup-account/type.ts
@@ -0,0 +1,11 @@
+import { AccountDetailProps } from "@/shared/contracts/account/account-details.contract"
+
+export const setupAccountDefualtValues: AccountDetailProps = {
+  email: '',
+  username: '',
+  fullname: '',
+  phone: '',
+  address: '',
+  password: '',
+  confirmPassword: '',
+}
